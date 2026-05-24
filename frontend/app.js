d9d166116ac487db87d1059c2fe74e8e801fde4f
saumek <samuelbitner06@gmail.com>
2026-05-24 11:25:05 +0200

    Frontend - wyswietlanie zdjęć w galeri oraz zdjecia na pelnym ekranie

--- frontend/app.js
+++ frontend/app.js
@@ -59,7 +59,7 @@ async function load_files(){
 function get_file_html(id,name,desc){
     return `
       <button class="group relative min-w-32 w-32 aspect-square overflow-hidden rounded-2xl border-gray-800 border-1 hover:scale-105 transition-all duration-100" onclick="openFile('${name}')">
-        <img src="images/${name}" alt="${desc}" fileId=${id}>
+        <img src="photos/${name}" alt="${desc}" fileId=${id} class="absolute top-0 min-h-[100%] min-w-[100%] object-cover">
         <div class="absolute bottom-0 right-0 left-0 bg-[#00000045] group-hover:opacity-100 opacity-0 transition-all duration-150">${desc}</div>
       </button>`
 }
@@ -91,10 +91,13 @@ function openFile(name){
 
     document.querySelector("body").innerHTML+=`
         <div filePreview class="z-30 absolute flex flex-row top-0 right-0 bg-[#000000bb] left-0 bottom-0 align-middle justify-around items-center pb-30">
-          <img class="relative bg-gray-800 max-w-[80vw] max-h-[90dvh] rounded-4xl" src="images/${name}">
+          <img class="relative bg-gray-800 max-w-[80vw] max-h-[90dvh] rounded-4xl" src="photos/${name}">
           </img>
           <button onclick="closePreview()" class="absolute top-8 right-10 hover:bg-gray-700 text-center aspect-square w-11 rounded-full border-gray-700 transition-all duration-75 border-1"> 
             <div class="absolute top-0 left-0 right-0 bottom-0 text-center text-3xl">x</div>
+          </button>
+          <button class="absolute top-8 right-23 border-1 fill-red-100 hover:bg-gray-700 border-gray-700 text-center aspect-square w-11 rounded-full transition-all duration-75 hover:scale-120 hover:fill-red-500 hover:rotate-12">
+            <svg class="" viewBox="-2.5 0 61 61" xmlns="http://www.w3.org/2000/svg"><defs><filter id="a" width="200%" height="200%" x="-50%" y="-50%" filterUnits="objectBoundingBox"><feOffset dy="1" in="SourceAlpha" result="shadowOffsetOuter1"/><feGaussianBlur stdDeviation="10" in="shadowOffsetOuter1" result="shadowBlurOuter1"/><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.1 0" in="shadowBlurOuter1" result="shadowMatrixOuter1"/><feMerge><feMergeNode in="shadowMatrixOuter1"/><feMergeNode in="SourceGraphic"/></feMerge></filter></defs><path fill-rule="evenodd" d="M36 26v10.997c0 1.659-1.337 3.003-3.009 3.003h-9.981c-1.662 0-3.009-1.342-3.009-3.003v-10.997h16zm-2 0v10.998c0 .554-.456 1.002-1.002 1.002h-9.995c-.554 0-1.002-.456-1.002-1.002v-10.998h12zm-9-5c0-.552.451-1 .991-1h4.018c.547 0 .991.444.991 1 0 .552-.451 1-.991 1h-4.018c-.547 0-.991-.444-.991-1zm0 6.997c0-.551.444-.997 1-.997.552 0 1 .453 1 .997v6.006c0 .551-.444.997-1 .997-.552 0-1-.453-1-.997v-6.006zm4 0c0-.551.444-.997 1-.997.552 0 1 .453 1 .997v6.006c0 .551-.444.997-1 .997-.552 0-1-.453-1-.997v-6.006zm-6-5.997h-4.008c-.536 0-.992.448-.992 1 0 .556.444 1 .992 1h18.016c.536 0 .992-.448.992-1 0-.556-.444-1-.992-1h-4.008v-1c0-1.653-1.343-3-3-3h-3.999c-1.652 0-3 1.343-3 3v1z" filter="url(#a)"/></svg>
           </button>
         </div>
       `
